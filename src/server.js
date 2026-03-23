52177b1b9084ee55e4ce30e853f373d5b6689da7
Serhii Kutovyi <wagenrex@gmail.com>
2026-03-23 14:02:38 +0200

    test 2

--- src/server.js
+++ src/server.js
@@ -1,2 +1,42 @@
-const message = "helllllloooo"
-console.log(message);
+import http from 'node:http';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const carsPath = path.join(import.meta.dirname, '..', 'cars.json');
+const carsJson = await fs.readFile(carsPath, 'utf-8');
+const cars = JSON.parse(carsJson);
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/cars') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(cars));
+  }
+
+  if (req.method === 'GET' && req.url.startsWith('/cars/')) {
+    const carId = req.url.split('/')[2];
+    const car = cars.find((car) => car.id === carId);
+
+    if (!car) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify({ message: 'Car not found' }));
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(car));
+  }
+
+  if (req.method === 'GET' && req.url === '/brands') {
+    res.statusCode = 200;
+    return res.end('all brands');
+  }
+
+  res.statusCode = 404;
+  res.end('not found');
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on localhost: 3000');
+});
