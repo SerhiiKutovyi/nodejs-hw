bf904213f0c5c1d20fcfcc89a922bba313df2923
Serhii Kutovyi <wagenrex@gmail.com>
2026-03-23 19:50:54 +0200

    HW express

--- src/server.js
+++ src/server.js
@@ -1,42 +1,71 @@
-import http from 'node:http';
-import path from 'node:path';
-import fs from 'node:fs/promises';
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
-const carsPath = path.join(import.meta.dirname, '..', 'cars.json');
-const carsJson = await fs.readFile(carsPath, 'utf-8');
-const cars = JSON.parse(carsJson);
+const app = express();
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/cars') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify(cars));
-  }
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
-  if (req.method === 'GET' && req.url.startsWith('/cars/')) {
-    const carId = req.url.split('/')[2];
-    const car = cars.find((car) => car.id === carId);
+// GET/notes
 
-    if (!car) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'application/json');
-      return res.end(JSON.stringify({ message: 'Car not found' }));
-    }
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify(car));
-  }
-
-  if (req.method === 'GET' && req.url === '/brands') {
-    res.statusCode = 200;
-    return res.end('all brands');
-  }
-
-  res.statusCode = 404;
-  res.end('not found');
+app.get('/notes', (req, res) => {
+  res.status(200).json({ meaassge: 'Retrieved all notes' });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on localhost: 3000');
+//GET/notes/:noteID
+
+app.get('/notes/:noteId', (req, res) => {
+  console.log(req.params);
+
+  res
+    .status(200)
+    .json({ meaassge: `Retrieved note with ID:${req.params.noteId}` });
+});
+
+//GET/test-error
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+// 404
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Rout not found' });
+});
+
+//error middlewear
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+    console.error('Error:', err.message);
+
+  res.status(500).json({
+    message: isProd ? 'Щось пішло не так' : err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
