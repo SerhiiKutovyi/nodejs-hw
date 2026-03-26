96bc6877c1baeeeaab6bed934ba0b81eb5959298
Serhii Kutovyi <wagenrex@gmail.com>
2026-03-26 17:41:33 +0200

    added routes

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,10 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({ message: err.message || err.name });
+  }
+
   const isProd = process.env.NODE_ENV === 'production';
   console.error('Error:', err.message);
 
--- src/server.js
+++ src/server.js
@@ -15,8 +15,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
 app.use(logger());
+app.use(
+  express.json({
+    limit: '200kb',
+  }),
+);
 
 app.use(notesRoutes);
 
